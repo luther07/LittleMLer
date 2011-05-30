7a04cd3df920ef4f20e7e1ce42a502b24ad3b08a
Mark Johnson <luther07@gmail.com>
2011-05-30 13:10:27 -0400

    refactor

--- meals/add_a_steak.ml
+++ meals/add_a_steak.ml
@@ -1,10 +1,4 @@
 #print_depth 20;;
 let rec (add_a_steak : meza -> meza * main) = function
-     (Shrimp)
-      -> (Shrimp,Steak)
-    |(Calamari)
-      -> (Calamari,Steak)
-    |(Escargots)
-      -> (Escargots,Steak)
-    |(Hummus)
-      -> (Hummus,Steak)
+     (x)
+      -> (x,Steak)
