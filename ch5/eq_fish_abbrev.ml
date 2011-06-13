bbaf9275826f9986ad5e1aad5b1e233254b339c4
Mark Johnson <luther07@gmail.com>
2011-06-13 07:36:17 -0400

    concise definition of eq_fish function, You know what I'm saying

---
+++ ch5/eq_fish_abbrev.ml
@@ -0,0 +1,11 @@
+#print_depth 20;;
+let rec (eq_fish_abbrev : (fish*fish) -> bool) = 
+  function
+     (Anchovy,Anchovy)
+      -> true
+    |(Lox,Lox)
+      -> true
+    |(Tuna,Tuna)
+      -> true
+    |(a_fish,another_fish)
+      -> false
