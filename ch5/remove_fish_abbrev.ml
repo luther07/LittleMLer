7cc17c9dbeaab9d0d32a76da79ef7f9815c26d84
Mark Johnson <luther07@gmail.com>
2011-06-13 07:16:43 -0400

    more concise definition of remove_fish function, You know what I'm saying

---
+++ ch5/remove_fish_abbrev.ml
@@ -0,0 +1,17 @@
+#print_depth 20;;
+let rec (remove_fish_abbrev : (fish * (fish pizza) -> fish pizza)) =
+   function
+      (x, Bottom)
+       -> Bottom
+     |(Tuna, Topping(Tuna,p))
+       -> remove_fish_abbrev(Tuna,p)
+     |(Tuna, Topping(t,p))
+       -> Topping(t,remove_fish_abbrev(Tuna,p))
+     |(Anchovy, Topping(Anchovy,p))
+       -> remove_fish_abbrev(Anchovy,p)
+     |(Anchovy, Topping(t,p))
+       -> Topping(t,remove_fish_abbrev(Anchovy,p))
+     |(Lox, Topping(Lox,p))
+       -> remove_fish_abbrev(Lox,p)
+     |(Lox, Topping(t,p))
+       -> Topping(t,remove_fish_abbrev(Lox,p))
