554a36e9fb317ef09feed17ddb67d33958e5f3f7
Mark Johnson <luther07@gmail.com>
2011-07-06 07:58:56 -0400

    working on syntax error, not fixed yet

--- ch6/soccurs.ml
+++ ch6/soccurs.ml
@@ -1,14 +1,14 @@
 #print_depth 20;;
-let rec (soccurs : fruit*(fruit slist) -> int) = 
+let rec soccurs = 
    function
       (a,Empty)
        -> 0
      |(a,Scons(s,y))
        -> soccurs(a,s) + soccurs(a,y)
-and
+   and
       (a,An_atom(b))
        -> if eq_fruit(a,b)
              then 1
-             else 0
-      (a,A_slist(y))
+          else 0
+     |(a,A_slist(y))
        -> soccurs(a,A_slist(y))
