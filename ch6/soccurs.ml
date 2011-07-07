de03885f8b651860dc67fbc41525ff34749bfcb8
Mark Johnson <luther07@gmail.com>
2011-07-06 20:46:15 -0400

    fixed syntax and form per ocaml-3.12-refman; like form: let rec name1 = fun...and name2 = fun in expr

--- ch6/soccurs.ml
+++ ch6/soccurs.ml
@@ -1,14 +1,15 @@
 #print_depth 20;;
-let rec soccurs = 
+let rec occurs_in_slist = 
    function
       (a,Empty)
        -> 0
      |(a,Scons(s,y))
-       -> soccurs(a,s) + soccurs(a,y)
-   and
+       -> occurs_in_sexp(a,s) + occurs_in_slist(a,y)
+and occurs_in_sexp =
+   function
       (a,An_atom(b))
        -> if eq_fruit(a,b)
              then 1
           else 0
      |(a,A_slist(y))
-       -> soccurs(a,A_slist(y))
+       -> occurs_in_slist(a,y)
