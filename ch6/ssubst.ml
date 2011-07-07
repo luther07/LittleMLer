762c2b043412bed99c456e970284d4e9dfc4a437
Mark Johnson <luther07@gmail.com>
2011-07-07 07:25:43 -0400

    function to substitute an item from this mutually self referential datatype

---
+++ ch6/ssubst.ml
@@ -0,0 +1,15 @@
+#print_depth 20;;
+let rec (subst_in_slist : fruit*fruit*(fruit slist) -> fruit slist) = 
+   function
+      (n,a,Empty)
+       -> Empty
+     |(n,a,Scons(s,y))
+       -> Scons(subst_in_sexp(a,s),subst_in_slist(a,y))
+and (subst_in_sexp : fruit*fruit*(fruit sexp) -> fruit sexp) =
+   function
+      (n,a,An_atom(b))
+       -> if eq_fruit(a,b)
+             then An_atom(n)
+          else An_atom(b)
+     |(n,a,A_slist(y))
+       -> A_slist(subst_in_slist(n,a,y))
