28406917bdd12b18760392dd2e4f42500034f757
Mark Johnson <luther07@gmail.com>
2011-07-08 07:38:47 -0400

    finishing touches on function on a mutually self referential datatype

--- ch6/sremove.ml
+++ ch6/sremove.ml
@@ -1,16 +1,15 @@
 #print_depth 20;;
-let rec (sremove_in_slist : fruit*fruit*(fruit slist) -> fruit slist) 
-= 
+let rec (remove_from_slist : fruit*(fruit slist) -> fruit slist) = 
    function
       (a,Empty)
        -> Empty
      |(a,Scons(s,y))
-       -> (* ? *) 
-and (remove_in_sexp : fruit*fruit*(fruit sexp) -> fruit sexp) =
+       -> if eq_fruit_in_atom(a,s)
+             then remove_from_slist(a,y)
+             else Scons(remove_from_sexp(a,s),remove_from_slist(a,y))
+and (remove_from_sexp : fruit*(fruit sexp) -> fruit sexp) =
    function
       (a,An_atom(b))
-       -> if eq_fruit(a,b)
-             then Empty
-          else An_atom(b)
+       -> An_atom(b)
      |(a,A_slist(y))
-       -> (* ? *)
+       -> A_slist(remove_from_slist(a,y))
